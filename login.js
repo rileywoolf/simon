a6a7ef92d6db493ab1205fbc544f6a3e69afe1e2
Riley Woolf <rileyawoolf@gmail.com>
2023-03-04 14:07:31 -0700

    fix spelling error

--- login.js
+++ login.js
@@ -1,6 +1,6 @@
 // When a user enters their login name, that name is put in the browser's local storage so it is available next time they play.
 function login() {
-  const nameE1 = document.querySelector("#name");
-  localStorage.setItem("userName", nameE1.value);
+  const nameEl = document.querySelector("#name");
+  localStorage.setItem("userName", nameEl.value);
   window.location.href = "play.html";
 }
