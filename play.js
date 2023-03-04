01c2847d193e2afca1930544c8911e7e327bd5bb
Riley Woolf <rileyawoolf@gmail.com>
2023-03-04 15:27:00 -0700

    stubbed game class

--- play.js
+++ play.js
@@ -34,3 +34,48 @@ class Button {
     });
   }
 }
+
+class Game {
+  buttons;
+  allowPlayer;
+  sequence;
+  playerPlaybackPos;
+  mistakeSound;
+
+  constructor() {
+    this.buttons = new Map();
+    this.allowPlayer = false;
+    this.sequence = [];
+    this.playerPlaybackPos = 0;
+    this.mistakeSound = loadSound("error.mp3");
+
+    document.querySelectorAll(".game-button").forEach((el, i) => {
+      if (i < btnDescriptions.length) {
+        this.buttons.set(el.id, new Button(btnDescriptions[i], el));
+      }
+    });
+
+    const playerNameEl = document.querySelector(".player-name");
+    playerNameEl.textContent = this.getPlayerName();
+  }
+
+  async pressButton(button) {}
+
+  async reset() {}
+
+  getPlayerName() {}
+
+  async playSequence() {}
+
+  addButton() {}
+
+  updateScore(score) {}
+
+  async buttonDance(laps = 1) {}
+
+  getRandomButton() {}
+
+  saveScore(score) {}
+
+  updateScores(userName, score, scores) {}
+}
