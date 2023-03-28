67e22a8d651559885a8020746dc2412850b41920
Riley Woolf <89953396+rileywoolf@users.noreply.github.com>
2023-03-27 22:25:00 -0600

    simon login (had to just copy the entire ex proj)

--- public/play.js
+++ public/play.js
@@ -1,10 +1,8 @@
-// Controls all game interactions and stores resulting score in browser's local storage.
-
 const btnDescriptions = [
-  { file: "sound1.mp3", hue: 120 },
-  { file: "sound2.mp3", hue: 0 },
-  { file: "sound3.mp3", hue: 60 },
-  { file: "sound4.mp3", hue: 240 },
+  { file: 'sound1.mp3', hue: 120 },
+  { file: 'sound2.mp3', hue: 0 },
+  { file: 'sound3.mp3', hue: 60 },
+  { file: 'sound4.mp3', hue: 240 },
 ];
 
 class Button {
@@ -26,6 +24,7 @@ class Button {
     this.paint(25);
   }
 
+  // Work around Safari's rule to only play sounds if given permission.
   async play(volume = 1.0) {
     this.sound.volume = volume;
     await new Promise((resolve) => {
@@ -47,15 +46,15 @@ class Game {
     this.allowPlayer = false;
     this.sequence = [];
     this.playerPlaybackPos = 0;
-    this.mistakeSound = loadSound("error.mp3");
+    this.mistakeSound = loadSound('error.mp3');
 
-    document.querySelectorAll(".game-button").forEach((el, i) => {
+    document.querySelectorAll('.game-button').forEach((el, i) => {
       if (i < btnDescriptions.length) {
         this.buttons.set(el.id, new Button(btnDescriptions[i], el));
       }
     });
 
-    const playerNameEl = document.querySelector(".player-name");
+    const playerNameEl = document.querySelector('.player-name');
     playerNameEl.textContent = this.getPlayerName();
   }
 
@@ -64,7 +63,6 @@ class Game {
       this.allowPlayer = false;
       await this.buttons.get(button.id).press(1.0);
 
-      // See if the user selects the correct button.
       if (this.sequence[this.playerPlaybackPos].el.id === button.id) {
         this.playerPlaybackPos++;
         if (this.playerPlaybackPos === this.sequence.length) {
@@ -75,7 +73,6 @@ class Game {
         }
         this.allowPlayer = true;
       } else {
-        // If they didn't, save their score, play the mistake sound, and have the buttons do their little dance.
         this.saveScore(this.sequence.length - 1);
         this.mistakeSound.play();
         await this.buttonDance(2);
@@ -83,24 +80,21 @@ class Game {
     }
   }
 
-  // Resets all of the data for a new game, plays button dance, adds the first button in the sequence and shows it to the user.
   async reset() {
     this.allowPlayer = false;
     this.playerPlaybackPos = 0;
     this.sequence = [];
-    this.updateScore("--");
+    this.updateScore('--');
     await this.buttonDance(1);
     this.addButton();
     await this.playSequence();
     this.allowPlayer = true;
   }
 
-  // Gets the player name from local storage.
   getPlayerName() {
-    return localStorage.getItem("userName") ?? "Mystery Player";
+    return localStorage.getItem('userName') ?? 'Mystery player';
   }
 
-  // Shows the player the sequence of buttons they need to press.
   async playSequence() {
     await delay(500);
     for (const btn of this.sequence) {
@@ -109,19 +103,16 @@ class Game {
     }
   }
 
-  // Adds a random button to the sequence the player needs to copy.
   addButton() {
     const btn = this.getRandomButton();
     this.sequence.push(btn);
   }
 
-  // Modifies the score displayed on the page.
   updateScore(score) {
-    const scoreEl = document.querySelector("#score");
+    const scoreEl = document.querySelector('#score');
     scoreEl.textContent = score;
   }
 
-  // Goes through the buttons and lights them up. Loops laps times.
   async buttonDance(laps = 1) {
     for (let step = 0; step < laps; step++) {
       for (const btn of this.buttons.values()) {
@@ -130,36 +121,35 @@ class Game {
     }
   }
 
-  // Randomly chooses a button from the list of buttons.
   getRandomButton() {
     let buttons = Array.from(this.buttons.values());
     return buttons[Math.floor(Math.random() * this.buttons.size)];
   }
 
-  // Save the final score of the game to the scores list.
   async saveScore(score) {
     const userName = this.getPlayerName();
     const date = new Date().toLocaleDateString();
     const newScore = { name: userName, score: score, date: date };
 
     try {
-      const response = await fetch("/api/score", {
-        method: "POST",
-        headers: { "content-type": "application/json" },
+      const response = await fetch('/api/score', {
+        method: 'POST',
+        headers: { 'content-type': 'application/json' },
         body: JSON.stringify(newScore),
       });
 
+      // Store what the service gave us as the high scores
       const scores = await response.json();
-      localStorage.setItem("scores", JSON.stringify(scores));
+      localStorage.setItem('scores', JSON.stringify(scores));
     } catch {
+      // If there was an error then just track scores locally
       this.updateScoresLocal(newScore);
     }
   }
 
   updateScoresLocal(newScore) {
     let scores = [];
-    const scoresText = localStorage.getItem("scores");
-    // If there are already scores saved, parse them.
+    const scoresText = localStorage.getItem('scores');
     if (scoresText) {
       scores = JSON.parse(scoresText);
     }
@@ -181,8 +171,7 @@ class Game {
       scores.length = 10;
     }
 
-    // After adding the new score to the list, save the updated list in local storage.
-    localStorage.setItem("scores", JSON.stringify(scores));
+    localStorage.setItem('scores', JSON.stringify(scores));
   }
 }
 
@@ -190,12 +179,10 @@ const game = new Game();
 
 function delay(milliseconds) {
   return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(true);
-    }, milliseconds);
+    setTimeout(resolve, milliseconds);
   });
 }
 
 function loadSound(filename) {
-  return new Audio("assets/" + filename);
+  return new Audio('assets/' + filename);
 }
--- public/scores.js
+++ public/scores.js
@@ -1,15 +1,15 @@
-// Retrieves the high scores from local storage so that they can be displayed.
-
 async function loadScores() {
   let scores = [];
   try {
-    const response = await fetch("/api/scores");
+    // Get the latest high scores from the service
+    const response = await fetch('/api/scores');
     scores = await response.json();
 
-    // Put scores in local storage in case we go offline.
-    localStorage.setItem("scores", JSON.stringify(scores));
+    // Save the scores in case we go offline in the future
+    localStorage.setItem('scores', JSON.stringify(scores));
   } catch {
-    const scoresText = localStorage.getItem("scores");
+    // If there was an error then just use the last saved scores
+    const scoresText = localStorage.getItem('scores');
     if (scoresText) {
       scores = JSON.parse(scoresText);
     }
@@ -19,24 +19,22 @@ async function loadScores() {
 }
 
 function displayScores(scores) {
-  const tableBodyEl = document.querySelector("#scores");
+  const tableBodyEl = document.querySelector('#scores');
 
   if (scores.length) {
+    // Update the DOM with the scores
     for (const [i, score] of scores.entries()) {
-      // Create the elements.
-      const positionTdEl = document.createElement("td");
-      const nameTdEl = document.createElement("td");
-      const scoreTdEl = document.createElement("td");
-      const dateTdEl = document.createElement("td");
+      const positionTdEl = document.createElement('td');
+      const nameTdEl = document.createElement('td');
+      const scoreTdEl = document.createElement('td');
+      const dateTdEl = document.createElement('td');
 
-      // Set the elements.
       positionTdEl.textContent = i + 1;
       nameTdEl.textContent = score.name;
       scoreTdEl.textContent = score.score;
       dateTdEl.textContent = score.date;
 
-      // Append the elements to the HTML.
-      const rowEl = document.createElement("tr");
+      const rowEl = document.createElement('tr');
       rowEl.appendChild(positionTdEl);
       rowEl.appendChild(nameTdEl);
       rowEl.appendChild(scoreTdEl);
@@ -45,8 +43,7 @@ function displayScores(scores) {
       tableBodyEl.appendChild(rowEl);
     }
   } else {
-    tableBodyEl.innerHTML =
-      "<tr><td colSpan=4>Be the first to score!</td></tr>";
+    tableBodyEl.innerHTML = '<tr><td colSpan=4>Be the first to score</td></tr>';
   }
 }
 
